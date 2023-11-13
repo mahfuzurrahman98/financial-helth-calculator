5f82f64d53820ee63e77fb184888147c633ee65b
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2023-11-13 23:11:05 +0600

    new types added for finance module

--- client/src/types.ts
+++ client/src/types.ts
@@ -26,3 +26,40 @@ export type errorType = {
   message: string;
   description: string;
 };
+
+export type FinanceType = {
+  id: number;
+  income: number;
+  expense: number;
+  debts: number;
+  assets: number;
+  score: number | null;
+};
+
+export type HistoryType = FinanceType & {
+  calculated_at: string;
+};
+
+export type DashboardDataType = {
+  recent_finances: FinanceType[];
+  total: number;
+  total_income: number;
+  total_expense: number;
+  last_debts: number;
+  last_assets: number;
+  total_score: number;
+  average_score: number;
+};
+
+export type FinaceFormData = {
+  income: string;
+  expense: string;
+  debts: string;
+  assets: string;
+  score: string;
+};
+
+export type statusType = {
+  loading: boolean;
+  error: null | number;
+};
