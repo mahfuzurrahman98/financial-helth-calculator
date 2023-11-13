35d7ee27e041b99c44bbe6150b0a9294d4633a37
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2023-11-13 23:10:18 +0600

    /me route removed

--- client/src/router/routes.ts
+++ client/src/router/routes.ts
@@ -6,7 +6,6 @@ import Create from '../pages/finances/Create';
 import Dashboard from '../pages/finances/Dashboard';
 import History from '../pages/finances/History';
 import Show from '../pages/finances/show';
-import Me from '../pages/profile/Me';
 
 
 import { RouteType } from '../types';
@@ -17,10 +16,9 @@ const routes: RouteType[] = [
 
   { path: '/login', element: Login, _protected: 0 },
   { path: '/register', element: Register, _protected: 0 },  
-  { path: '/me', element: Me, _protected: 1 },
 
   { path: '/dashboard', element: Dashboard, _protected: 1 },
-  { path: '/finances/calcuate', element: Create, _protected: 1 },
+  { path: '/finances/calculate', element: Create, _protected: 1 },
   { path: '/finances/:id', element: Show, _protected: 1 },  
   { path: '/finances/history', element: History, _protected: 1 },
 ];
