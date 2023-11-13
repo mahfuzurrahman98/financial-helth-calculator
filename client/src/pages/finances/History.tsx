be5dc769daf74454481b8647e9773e493040ee41
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2023-11-13 23:14:06 +0600

    a page to show the full finance history

--- client/src/pages/finances/History.tsx
+++ client/src/pages/finances/History.tsx
@@ -1,12 +1,121 @@
-
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import ComponentLoader from '../../components/ComponentLoader';
+import useAxiosPrivate from '../../hooks/useAxiosPrivate';
+import { HistoryType, statusType } from '../../types';
+import RootLayout from '../RootLayout';
 
 const History = () => {
+  const [historyData, setHistoryData] = useState<HistoryType[]>([]);
+  const [status, setStatus] = useState<statusType>({
+    loading: true,
+    error: null,
+  });
+
+  const axiosPrivate = useAxiosPrivate();
+
+  const getHistoryData = async () => {
+    try {
+      const response = await axiosPrivate.get('/finances');
+      const data = response.data;
+      const respData = data.data;
+
+      console.log(respData);
+      setHistoryData(respData.finances);
+      setStatus({
+        loading: false,
+        error: null,
+      });
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    getHistoryData();
+  }, []);
+
   return (
-    <div>
-      <h1>History</h1>
-    </div>
+    <ComponentLoader
+      status={status}
+      component={
+        <RootLayout>
+         <h1 className="text-3xl font-semibold border-b-2 border-b-gray-600">
+            Finance History
+          </h1>
+
+          <div className="">
+            <Link
+              to="/finances/calculate"
+              className="float-right bg-green-700 text-white px-3 py-1 rounded-md text-md inline-block"
+            >
+              <span
+                className="font-bold mr-1"
+                dangerouslySetInnerHTML={{ __html: '&plus;' }}
+              />
+              Calculate New Finance
+            </Link>
+          </div>
+
+          <table className="w-full table-auto">
+            <thead>
+              <tr>
+                <th className="text-left border border-gray-400 px-2 py-2">
+                  Income
+                </th>
+                <th className="text-left border border-gray-400 px-2 py-2">
+                  Expense
+                </th>
+                <th className="text-left border border-gray-400 px-2 py-2">
+                  Debts
+                </th>
+                <th className="text-left border border-gray-400 px-2 py-2">
+                  Assets
+                </th>
+                <th className="text-left border border-gray-400 px-2 py-2">
+                  Score
+                </th>
+                <th className="text-justify border border-gray-400 px-2 py-2">
+                  Date
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {historyData.map((finance: HistoryType, key: number) => (
+                <tr key={key}>
+                  <td className="border border-gray-400 px-2 py-2">
+                    ${finance.income}
+                  </td>
+                  <td className="border border-gray-400 px-2 py-2">
+                    ${finance.expense}
+                  </td>
+                  <td className="border border-gray-400 px-2 py-2">
+                    ${finance.debts}
+                  </td>
+                  <td className="border border-gray-400 px-2 py-2">
+                    ${finance.assets}
+                  </td>
+                  <td className="border border-gray-400 px-2 py-2">
+                    {finance.score?.toFixed(2)}
+                  </td>
+                  <td className="border border-gray-400 px-2 py-2">
+                    {finance.calculated_at}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+
+          <div className="mt-4">
+            <Link
+              to="/dashboard"
+              className="underline text-green-700 font-semibold"
+            >
+              Back to Dashboard
+            </Link>
+          </div>
+        </RootLayout>
+      }
+    />
   );
 };
-
 
 export default History;
