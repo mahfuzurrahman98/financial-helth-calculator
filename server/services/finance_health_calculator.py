ed24f72a5b4c454424c9652ebefdb7147eb132e2
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2023-11-14 00:44:48 +0600

    modified algorithm to calculate the score

--- server/services/finance_health_calculator.py
+++ server/services/finance_health_calculator.py
@@ -1,17 +1,44 @@
-
-from os import environ
-from fastapi import FastAPI, HTTPException
-
 def finance_health_calculator(
     income: float,
     expense: float,
     debts: float,
     assets: float
 ):
-    # calculate the finance health score of a company using its monthly income, expense, debts, and assets
-    # return a score between 0 and 100
-    # 0 is the worst score, 100 is the best score
-    # the score is calculated using the following formula:
-    # score = (income - expense) / (debts + assets)
-    score = (income - expense) / (debts + assets)
-    return score
+    disposable_income = income - expense
+    debt_to_income_ratio = (debts / income) * 100
+    savings_rate = ((income - expense) - debts) / income * 100
+    net_worth = assets - debts
+
+    normalized_scores = {
+        'disposable_income': min(100, max(0, (disposable_income / max(income, 1)) * 100)),
+        'debt_to_income_ratio': min(100, max(0, 100 - debt_to_income_ratio)),
+        'savings_rate': min(100, max(0, savings_rate)),
+        'net_worth': min(100, max(0, (net_worth / max(assets, 1)) * 100)),
+    }
+
+    weights = {
+        'disposable_income': 0.4,
+        'debt_to_income_ratio': 0.4,
+        'savings_rate': 0.2,
+        'net_worth': 0.2,
+    }
+
+    financial_health_score = sum(
+        score * weights[metric] for metric, score in normalized_scores.items()
+    )
+
+    return financial_health_score
+
+def categorize_financial_health_score(score):
+    if 80 <= score <= 100:
+        return "Excellent"
+    elif 60 <= score < 80:
+        return "Good"
+    elif 40 <= score < 60:
+        return "Stable"
+    elif 20 <= score < 40:
+        return "Challenged"
+    elif 0 <= score < 20:
+        return "Critical"
+    else:
+        return "Invalid Score"
