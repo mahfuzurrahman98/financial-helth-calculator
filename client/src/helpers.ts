1b8d0351915995e73dddb48666191d493f3a03fe
Md Mahfuzur Rahman Arif <mdmahfuzurrahmanarif@gmail.com>
2023-11-14 22:34:23 +0600

    added another metric to categorized the score

--- client/src/helpers.ts
+++ client/src/helpers.ts
@@ -15,15 +15,17 @@ function categorizeHealthScore(score: number) {
 }
 
 function categorizeHealthScoreColor(score: number) {
-  if (score >= 80 && score <= 100) {
+  if (score >= 85 && score <= 100) {
     return 'bg-green-700';
-  } else if (score >= 60 && score < 80) {
+  } else if (score >= 65 && score < 85) {
     return 'bg-green-500';
-  } else if (score >= 40 && score < 60) {
-    return 'bg-yellow-700';
-  } else if (score >= 20 && score < 40) {
-    return 'bg-yellow-500';
-  } else if (score >= 0 && score < 20) {
+  } else if (score >= 45 && score < 65) {
+    return 'bg-purple-600';
+  } else if (score >= 25 && score < 45) {
+    return 'bg-purple-400';
+  } else if (score >= 10 && score < 25) {
+    return 'bg-yellow-600';
+  } else if (score < 10) {
     return 'bg-red-600';
   } else {
     return 'bg-gray-500';
@@ -40,43 +42,59 @@ function generateSuggestions(
   const disposableIncome = income - expense;
   const debtToIncomeRatio = (debts / income) * 100;
   const netWorth = assets - debts;
-  netWorth;
+
   const customSuggestions = [];
 
-  if (score < 20) {
-    customSuggestions.push('You should consider filing for bankruptcy.');
-  } else if (score < 40) {
-    if (debtToIncomeRatio > 50) {
-      customSuggestions.push('You should consider paying off your debts.');
-    } else {
-      customSuggestions.push('You should consider increasing your income.');
-    }
-  } else if (score < 60) {
-    customSuggestions.push('You could do better.');
-
-    if (disposableIncome < 0) {
-      customSuggestions.push('You should consider increasing your income.');
-    } else {
-      if (debtToIncomeRatio > 50) {
-        customSuggestions.push('You should consider paying off your debts.');
-      } else {
-        customSuggestions.push('You should consider decreasing your expenses.');
-      }
-    }
-  } else if (score < 80) {
+  if (score < 10) {
     customSuggestions.push(
-      'You are just on the edge of being financially healthy.'
+      'Your financial health is critically low. You should consider filing for bankruptcy.'
     );
-
+  } else if (score < 25) {
+    customSuggestions.push(
+      'Your financial health is in a challenging state. Consider taking immediate actions.'
+    );
+  } else if (score < 45) {
     if (debtToIncomeRatio > 50) {
-      customSuggestions.push('Keep it up, and consider paying off your debts.');
+      customSuggestions.push(
+        'Your debt-to-income ratio is high. Consider paying off your debts.'
+      );
     } else {
       customSuggestions.push(
-        'Keep it up, and consider decreasing your expenses.'
+        'Your financial health is stable but needs improvement. Consider increasing your income.'
       );
     }
-  } else if (score < 100) {
-    customSuggestions.push('You are on the top of your game!');
+  } else if (score < 65) {
+    customSuggestions.push(
+      'Your financial health is decent, but there is room for improvement.'
+    );
+
+    if (disposableIncome < 0) {
+      customSuggestions.push(
+        'Your disposable income is negative. Consider increasing your income.'
+      );
+    } else {
+      if (debtToIncomeRatio > 50) {
+        customSuggestions.push(
+          'Your debt-to-income ratio is high. Consider paying off your debts.'
+        );
+      } else {
+        customSuggestions.push(
+          'Your financial health could improve by decreasing your expenses.'
+        );
+      }
+    }
+  } else if (score < 85) {
+    customSuggestions.push('You are on the edge of being financially healthy.');
+
+    if (debtToIncomeRatio > 50) {
+      customSuggestions.push('Keep it up! Consider paying off your debts.');
+    } else {
+      customSuggestions.push('Keep it up! Consider decreasing your expenses.');
+    }
+  } else if (score <= 100) {
+    customSuggestions.push(
+      'Congratulations! You are on top of your financial game!'
+    );
   }
 
   return customSuggestions;
@@ -87,4 +105,3 @@ export {
   categorizeHealthScoreColor,
   generateSuggestions
 };
-
